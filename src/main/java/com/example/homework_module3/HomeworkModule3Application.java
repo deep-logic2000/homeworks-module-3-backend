5e84ceaf08fe928f58230cfbb673cbe805ca76bc
Kirill Nazarov <urij7762@gmail.com>
2024-06-07 10:11:41 +0300

    initial commit

---
+++ src/main/java/com/example/homework_module3/HomeworkModule3Application.java
@@ -0,0 +1,13 @@
+package com.example.homework_module3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkModule3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkModule3Application.class, args);
+	}
+
+}
